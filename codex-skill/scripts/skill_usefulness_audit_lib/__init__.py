8ac0d5cf04f37d4124859ce81dbfdad2361dd258
gongyu0918-debug <gongyu0918@gmail.com>
2026-06-27 06:49:46 +0800

    Load minimal compatibility fixes during package init

--- codex-skill/scripts/skill_usefulness_audit_lib/__init__.py
+++ codex-skill/scripts/skill_usefulness_audit_lib/__init__.py
@@ -1,9 +1,21 @@
 from .constants import *
 from .common import *
+from .frontmatter_compat import patch_common_module as _patch_common_module
+from .frontmatter_compat import patch_namespace as _patch_common_namespace
+
+_patch_common_module()
+_patch_common_namespace(globals())
+
 from .usage_loader import *
 from .ablation import *
 from .community import *
 from .risk_quality import *
 from .scoring import *
 from .reporting import *
+from .reporting_compat import patch_reporting_module as _patch_reporting_module
+from .reporting_compat import patch_namespace as _patch_reporting_namespace
+
+_patch_reporting_module()
+_patch_reporting_namespace(globals())
+
 from .cli import *
